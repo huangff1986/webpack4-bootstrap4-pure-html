7c3d77422f09e4c62c0dcf3e6cdc1f7f74147aca
Zero <906032172@qq.com>
2019-01-13 06:08:02 +0800

    成功引入bootstrap 完成入口文件自动引入

--- webpack.config.js
+++ webpack.config.js
@@ -9,39 +9,30 @@ const webpack = require('webpack');
  * 获取入口
  */
 
-function getEntry() 
-{
-    let entry = [];
-    entry = glob.sync(path.resolve(__dirname, 'src/pages/*/*.js'))
+let entry = {}
+let htmlWebpackPlugins = []
 
-    return entry;
-}
+glob.sync('src/pages/*/*.js').forEach((page)=> {
 
-/**
- * 入口文件与html文件匹配
- */
-function getHtmlWebpackPlugins()
-{
-    let htmlWebpackPlugins = [];
-    let entry = getEntry();
-    
-    entry.foreach((page)=> {
-        let entryChunkName = page.split('/')[page.split('/').length-1].split('.')[0];
-        let entryTemplateHtml = page.slice(0,-3) + '.html';
+    let entryName = page.split('/')[page.split('/').length-1].split('.')[0];
+    let entryTemplateHtml = page.slice(0,-3) + '.html';
 
-        htmlWebpackPlugin.push(
-            new htmlWebpackPlugin({
-                template: entryTemplateHtml,
-                chunks: [entryChunkName],
-                filename: 'static/js/[name].js'
-            })
-        )
-    })
-}
+    entry[entryName] = path.resolve(__dirname, page);
+
+    htmlWebpackPlugins.push(
+        new htmlWebpackPlugin({
+            template: entryTemplateHtml,
+            chunks: [entryName],
+            filename: `${entryName}.html`
+        })
+    )
+})
+
+console.log(entry)
 
 module.exports = {
     mode: 'development',
-    entry: getEntry(),
+    entry,
     output: {
         filename: 'static/js/[name].js',
         path: path.resolve(__dirname, 'dist'),
@@ -67,6 +58,6 @@ module.exports = {
             'window.jquery': 'jquery',
             Popper: ['popper.js','default']
         }),
-        ...getHtmlWebpackPlugins()
+        ...htmlWebpackPlugins
     ]
 }
